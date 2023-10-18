c5b9dc0b1535639a10a46e514f5027df0d87af04
DesmondP06 <desmail86@gmail.com>
2023-10-18 09:12:25 -0400

    Variable size nav bar

--- navbar.js
+++ navbar.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").styles.padding = "30px 10px";
+    document.getElementById("logo").styles.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").styles.padding = "80px 10px";
+    document.getElementById("logo").styles.fontSize = "35px";
   }
 }
