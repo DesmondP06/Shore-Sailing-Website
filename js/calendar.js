ce300e65c3ea4f0990c0cd56fcbc913714e04fd5
andersn12 <anders.n.nielsen.nj@gmail.com>
2024-01-06 22:47:18 -0500

    pic edited

--- js/calendar.js
+++ js/calendar.js
@@ -8,7 +8,7 @@ const eventCards = document.getElementById('event-cards');
 
 let currentDate = new Date();
 
-let eventDates = ['Sat Jan 13 2024', 'Sat Jan 20 2024'];
+let eventDates = ['Sat Jan 13 2024'];
 
 const updateCalendar = () => {
     const currentYear = currentDate.getFullYear();
@@ -56,13 +56,13 @@ updateCalendar();
 
 prevBtn.addEventListener('click', () => {
     currentDate.setMonth(currentDate.getMonth() - 1);
-    /*eventCards.innerHTML = ``;*/
+    eventCards.innerHTML = ``;
     updateCalendar();
 })
 
 nextBtn.addEventListener('click', () => {
     currentDate.setMonth(currentDate.getMonth() + 1);
-    /*eventCards.innerHTML = ``;*/
+    eventCards.innerHTML = ``;
     updateCalendar();
 })
 
@@ -78,6 +78,7 @@ const updateEvents = () => {
             </div>
         </div>`;
     }
+    /*
     else if (event2Bool){
         eventCards.innerHTML = `
         <div class="card" style="width: 18rem;">
@@ -89,27 +90,28 @@ const updateEvents = () => {
             </div>
         </div>`;
     }
+    */
     else{
         eventCards.innerHTML = ``
     }
 }
 
 const event1Btn = document.getElementById('0 13');
-const event2Btn = document.getElementById('0 20');
+//const event2Btn = document.getElementById('0 20');
 
 
 
 let event1Bool = false;
-let event2Bool = false;
+//let event2Bool = false;
 
 event1Btn.addEventListener('click', () => {
     event1Bool = !event1Bool;
     if (event1Bool){
-        event2Bool = false;
+        //event2Bool = false;
     }
     updateEvents();
 })
-
+/*
 event2Btn.addEventListener('click', () => {
     event2Bool = !event2Bool;
     if (event2Bool){
@@ -117,4 +119,4 @@ event2Btn.addEventListener('click', () => {
     }
     updateEvents();
 })
-
+*/
